365ffce22d50a3e3dbb445b1dd728cf186039356
OscarDRT <oscarnetworkingpro@gmail.com>
2021-06-19 01:40:06 -0500

    add react native navigation

---
+++ src/navigation/index.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import { View, Text, TouchableOpacity } from 'react-native'
+
+export const Navigation = () => {
+  return (
+    <NavigationContainer>
+      <RootNavigator />
+    </NavigationContainer>
+  )
+}
+
+const Stack = createStackNavigator()
+
+const Home2 = () => {
+  return <View style={{ flex: 1, backgroundColor: 'red' }} />
+}
+
+const Home = ({ navigation }) => {
+  return (
+    <View style={{ flex: 1, backgroundColor: 'red' }}>
+      <TouchableOpacity onPress={() => navigation.navigate('Home2')}>
+        <Text>go home2</Text>
+      </TouchableOpacity>
+    </View>
+  )
+}
+
+const RootNavigator = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name={'Home'} component={Home} />
+      <Stack.Screen name={'Home2'} component={Home2} />
+    </Stack.Navigator>
+  )
+}
