6745737a8c71226b6088ac1cbd8be0ae938eb979
OscarDRT <oscarnetworkingpro@gmail.com>
2021-06-20 15:33:51 -0500

    remove default header

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -19,21 +19,9 @@ const Stack = createStackNavigator<RootStackParamList>()
 
 const RootNavigator = () => {
   return (
-    <Stack.Navigator>
-      <Stack.Screen
-        name={'PostsScreen'}
-        component={PostsScreen}
-        options={{
-          title: 'Posts',
-        }}
-      />
-      <Stack.Screen
-        name={'InternalPostScreen'}
-        component={InternalPostScreen}
-        options={{
-          title: 'Post',
-        }}
-      />
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name={'PostsScreen'} component={PostsScreen} />
+      <Stack.Screen name={'InternalPostScreen'} component={InternalPostScreen} />
     </Stack.Navigator>
   )
 }
